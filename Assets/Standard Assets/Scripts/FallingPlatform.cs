5a6c5dabbe57ae292ead9f87a13d319a8c8a309a
Brock <bbolt3@uis.edu>
2014-10-19 01:05:21 -0500

    Adjusted project gravity Began first enemy Added some additional prefabs Began Level 3

---
+++ Assets/Standard Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+	private void OnTriggerEnter2D (Collider2D other){
+		//Player hit switch to drop platform
+		if (other.gameObject.tag == "Player")
+			rigidbody2D.gravityScale = 10;
+		else if(other.gameObject.tag == "Enemy")
+			Destroy(other.gameObject); // enemy was smashed change to death sequence
+	}
+}
--- Assets/Standard Assets/Scripts/PlayerController.cs
+++ Assets/Standard Assets/Scripts/PlayerController.cs
@@ -7,20 +7,24 @@ public class PlayerController : MonoBehaviour {
 	private float deathHeight = -15f;  //Y value to determine player fell off map
 	private bool isFacingRight = true; 
 	private bool isJumping = false;
-	private float speed = 9f;  //static for now
-	private float jumpForce = 400f; // static for now
+	private float initialSpeed = 12f;
+	private float speed;  
+	private float jumpForce = 1000f; // static for now
 	private Animator anim;
 
 	// Use this for initialization
 	private void Start () {
 		anim = GetComponent<Animator>();
+		speed = initialSpeed;
 	}
 
 	//FixedUpdate can be called variable amount of times per frame
 	//Physics is generally suggested here
 	private void FixedUpdate(){
 		if (transform.position.y <= deathHeight) {
-			//call respawn or flash death graphic
+			//player died send back to spawn point 
+			Transform spawnpoint = GameObject.FindWithTag("SpawnPoint").transform;
+			transform.position = spawnpoint.position;
 		} else {
 
 			//determine if player is moving 
@@ -32,7 +36,7 @@ public class PlayerController : MonoBehaviour {
 			anim.SetFloat("HorizontalSpeed", Mathf.Abs(moveHorizontal));
 
 			//change player movement data here
-			rigidbody2D.velocity = new Vector2 (moveHorizontal * speed, rigidbody2D.velocity.y);
+			rigidbody2D.velocity = new Vector2 (moveHorizontal * speed, moveVertical);
 
 
 			// first case player moves right, not facing right
@@ -55,7 +59,13 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButtonDown("Jump") && !isJumping){
 			//add force to jump only in Y axis
 			rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+			speed = initialSpeed;
 		}
+		else if (Input.GetButton("Horizontal")){
+			speed += .08f;
+		}
+		else
+			speed = initialSpeed;
 	}
 
 	private void FlipSprite(){
