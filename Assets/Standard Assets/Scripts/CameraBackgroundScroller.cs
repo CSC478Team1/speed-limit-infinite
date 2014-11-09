41d357e5468df06839f85d7cf873c6630d7a2e0d
Brock <bbolt3@uis.edu>
2014-11-08 21:02:56 -0600

    Added Background Camera / Script Removed Camera from Player1 Prefab Added camera script to follow player and zoom in / out Added Input Manager commands for zoom in / zoom out Added Main Camera and Background Camera to Levels 1-5 Added additional testing images for background

---
+++ Assets/Standard Assets/Scripts/CameraBackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBackgroundScroller : MonoBehaviour {
+	private GameObject player;
+	private float quadWidth;
+	private float quadHeight;
+	private float scale = .001f;
+
+	private void Start(){
+		player = GameObject.Find("Player1");
+		GameObject camQuad = GameObject.Find("BackgroundQuad");
+		GameObject backgroundCamera = GameObject.Find("BackgroundCamera");
+		quadHeight = backgroundCamera.camera.orthographicSize * 3.0f;
+		quadWidth = quadHeight * Screen.width/Screen.height;
+		Vector3 quadScale =  new Vector3(quadWidth, quadHeight, 1f);
+		camQuad.transform.localScale = quadScale;
+
+
+	}
+	// Update is called once per frame
+	private void Update () {
+		renderer.material.mainTextureOffset = new Vector2((player.transform.position.x * scale) % quadWidth, ((player.transform.position.y + 15) * scale) % quadHeight);
+	
+	}
+}
---
+++ Assets/Standard Assets/Scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	private Transform player;
+
+	// Use this for initialization
+	private void Start () {
+		player = GameObject.Find("Player1").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = new Vector3(player.position.x, player.position.y, transform.position.z);
+		gameObject.transform.position = position;
+
+		if (Input.GetButtonDown("Camera Zoom In"))
+			if (gameObject.camera.fieldOfView < 100)
+				gameObject.camera.fieldOfView += 5;
+
+		if (Input.GetButtonDown("Camera Zoom Out"))
+			if (gameObject.camera.fieldOfView > 20)
+				gameObject.camera.fieldOfView -=5;
+	}
+}
