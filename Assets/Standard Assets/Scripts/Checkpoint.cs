899752cbcfb0eb370f9ab26745f7e689b5874cec
Brock <bbolt3@uis.edu>
2014-11-22 17:06:31 -0600

    Replaced floor texture / impassible areas with floortest6 (darkest one chosen). Updated all prefabs / areas that should indicate that they are impassible to the player.
    To update all of these in the future we can simply replace floortest6 instead of editing all the levels/prefabs again.

--- Assets/Standard Assets/Scripts/Checkpoint.cs
+++ Assets/Standard Assets/Scripts/Checkpoint.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class Checkpoint : MonoBehaviour {
 
+	private bool triggered = false;
 
 	private void OnTriggerEnter2D (Collider2D other){
-		if (other.tag == "Player"){
+		if (other.tag == "Player" && !triggered){
 			other.GetComponent<PlayerController>().SetCheckpoint(transform);
 			GameManager.DisplayMessage("Checkpoint Reached");
+			triggered = true;
 		}
 
 	}
